d6d3c8d3c28a4a7b1d0cc7c8a0638f2a62b901b5
trong-le <trong.p.le.92@gmail.com>
2015-12-27 15:37:59 -0500

    update unique user fields

--- Autolayout/ViewController.swift
+++ Autolayout/ViewController.swift
@@ -14,22 +14,35 @@ class ViewController: UIViewController
     @IBOutlet weak var loginField: UITextField!
     @IBOutlet weak var passwordField: UITextField!
     @IBOutlet weak var passwordLabel: UILabel!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var companyLabel: UILabel!
+    @IBOutlet weak var imageView: UIImageView!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         updateUI()
     }
     
+    var loggedInUser: User? { didSet { updateUI() } }
+    
     // Set boolean for security field. didSet by updateUI
     var secure: Bool = false { didSet { updateUI() } }
     
-    // Update password label depending on security bool
+    /* Update password label depending on security bool
+       update labels to users
+    */
     private func updateUI() {
         passwordField.secureTextEntry = secure
         passwordLabel.text = secure ? "Secured Password" : "Password"
+        nameLabel.text = loggedInUser?.name
+        companyLabel.text = loggedInUser?.company
+        imageView.image = loggedInUser?.image
     }
     
+    // Fill in 
     @IBAction func login() {
+        loggedInUser = User.login(loginField.text ?? "", password: passwordField.text ?? "")
     }
 
     // Show password security setting, changes security bool
@@ -38,3 +51,14 @@ class ViewController: UIViewController
     }
 }
 
+// Extend User to input image for unique user
+extension User
+{
+    var image: UIImage? {
+        if let image = UIImage(named: login) {
+            return image
+        } else {
+            return UIImage(named: "unknown_user")
+        }
+    }
+}
