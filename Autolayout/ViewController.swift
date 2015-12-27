1f1e2df778805f7da8924c751a8f0dbfb3b0c948
trong-le <trong.p.le.92@gmail.com>
2015-12-27 14:55:09 -0500

    Setup show password security and text

--- Autolayout/ViewController.swift
+++ Autolayout/ViewController.swift
@@ -8,9 +8,33 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController
+{
 
+    @IBOutlet weak var loginField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    @IBOutlet weak var passwordLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        updateUI()
+    }
+    
+    // Set boolean for security field. didSet by updateUI
+    var secure: Bool = false { didSet { updateUI() } }
+    
+    // Update password label depending on security bool
+    private func updateUI() {
+        passwordField.secureTextEntry = secure
+        passwordLabel.text = secure ? "Secured Password" : "Password"
+    }
+    
+    @IBAction func login() {
+    }
 
-
+    // Show password security setting, changes security bool
+    @IBAction func toggleSecurity() {
+        secure = !secure
+    }
 }
 
